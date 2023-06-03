8e52ab54795eb1d0d5943a0110f0468dcfb6d936
João pedro Maciel pinheiro <shionlk98@gmail.com>
2023-06-03 00:44:06 -0300

    update 2

--- Src/script.js
+++ Src/script.js
@@ -1,15 +1,33 @@
 const buttonClick = document.getElementById("button-toggle");
 const body = document.querySelector("body");
 const buttonvideo = document.querySelector(".video");
+const buttonImg = document.querySelector(".button-img")
+
 
 buttonClick.addEventListener("click", () => {
     
     const videoChange = body.classList.toggle(".video");
-    if (videoChange) {
+    const imgChange = body.classList.toggle(".button-img");
+    
+    if (videoChange ,imgChange) {
             buttonvideo.setAttribute("src", "/Src/Video/Tokito vs Gyokko .webm");
+            buttonImg.setAttribute("src", "/Src/img/oni-kanji.png");
+
             
           } else {
              buttonvideo.setAttribute("src", "/Src/Video/Tanjiro  vs Hantengu.webm");
+             buttonImg.setAttribute("src", "/Src/img/Slayer-removebg-preview.png");
+                    
            }
-  
-});
+  }  
+);
+
+// function changeImg () {
+//     alert ("oi")
+// }
+ 
+
+
+        
+   
+
